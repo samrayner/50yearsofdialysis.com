6b4fbb5b065d88ee5228dcf50b92b9de8ba68460
Sam Rayner <sam@terracoding.com>
2015-01-05 15:35:18 +0000

    Add IE fixes and alert bar

---
+++ source/assets/javascripts/ie.js
@@ -0,0 +1,12 @@
+var $buoop = {
+  i: 6,
+  reminder: 0,
+  text: '<strong>Looks like you\'re in Internet Explorer so things might look a little broken.</strong> If only the NHS would upgrade! This site will work better in <a href="http://www.google.com/chrome/">Google Chrome</a> if you have that installed :)'
+};
+function $buo_f(){
+ var e = document.createElement("script");
+ e.src = "//browser-update.org/update.js";
+ document.body.appendChild(e);
+};
+try {document.addEventListener("DOMContentLoaded", $buo_f,false)}
+catch(e){window.attachEvent("onload", $buo_f)}
